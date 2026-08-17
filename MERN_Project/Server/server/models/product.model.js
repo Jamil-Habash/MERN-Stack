29f26b6753b601a36947caf70f61a7e3ce75221d
Jamil-Habash <168636758+Jamil-Habash@users.noreply.github.com>
2026-08-17 15:37:20 +0300

    Delete MERN_Project/Server directory

--- MERN_Project/Server/server/models/product.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ProductSchema = new mongoose.Schema({
-    title:{type:String},
-    price:{type:String},
-    desc:{type:String}
-}, {timestamps: true});
-const Product = mongoose.model('Product', ProductSchema);
-module.exports = Product;
--- MERN_Project/Server/server/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const ProductController = require('../controllers/product.controller');
-module.exports = function(app){
-    app.post('/api/products', ProductController.createProduct);
-    app.get('/api/products', ProductController.getAllProducts);
-    app.get('/api/products/:id', ProductController.getProduct);
-    app.patch('/api/products/:id', ProductController.updateProduct);
-    app.patch('/api/products/:id/edit', ProductController.updateProduct);
-    app.delete('/api/products/:id', ProductController.deleteProduct);
-}
