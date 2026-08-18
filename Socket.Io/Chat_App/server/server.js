3016a13c012b314014c20c15de43758884d95549
jamhab35@gmail.com <jamhab35@gmail.com>
2026-08-18 12:25:02 +0300

    socket

---
+++ Socket.Io/Chat_App/server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require('./server/config/mongoose.config');
+
+const port = process.env.PORT;
+require('./server/routes/author.routes')(app);
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
